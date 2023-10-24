2cca22b9302c344a81ce55752685e3c2472f7eae
sauchau <saurav25398chauhan@gmail.com>
2023-10-24 15:33:59 +0530

    Add a function to play a single round of the game
    Compare the choices made by player and computer and return win, lose
    or draw string.

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ function getComputerChoice() {
 
     return randomComputerMove;
 }
+
+function playRound(playerSelection, computerSelection) {
+    const winText = `You Win! ${playerSelection} beats ${computerSelection}`;
+    const drawText = `Draw! Both played the same move`;
+    const loseText = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    
+    if ((playerSelection == "Rock" && computerSelection == "Scissor") ||
+     (playerSelection == "Paper" && computerSelection == "Rock") ||
+     (playerSelection == "Scissor" && computerSelection == "Paper")) {
+        return winText;
+    } else if (playerSelection == computerSelection) {
+        return drawText;
+    }
+
+    return loseText;
+}
